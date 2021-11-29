b192d4813f07b049c2c84136e5350cbbed5a203b
Richard Lane <richard.lane@bristol.ac.uk>
2021-11-29 11:05:43 +0000

    build pngs

--- tikz/build.sh
+++ tikz/build.sh
@@ -7,11 +7,19 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 IMG_DIR="$(dirname $SCRIPT_DIR)/img/"
 mkdir -p $IMG_DIR
 
+echo "Compiling pdfs"
 for filename in ${SCRIPT_DIR}/*.tex; do
-    pdflatex -halt-on-error -output-directory img $filename
+    pdflatex -halt-on-error -output-directory $IMG_DIR ${filename}
+done
+
+# Convert to pngs
+echo "converting to png"
+for filename in ${IMG_DIR}/*.pdf; do
+    convert -density 300 -quality 90 $filename ${filename%.*}.png
 done
 
 # Cleanup build artefacts
-for filename in ${IMG_DIR}/*.{aux,log}; do
+echo "cleanup"
+for filename in ${IMG_DIR}/*.{aux,log,pdf}; do
     rm $filename
 done
